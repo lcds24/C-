3442abb0837b5a9c65cc17d18aeba7063628addc
lcds <1228421675@qq.com>
2022-04-12 00:04:12 +0800

    temperature transform

---
+++ C++算法初级/模拟与高精度/temperature_transform.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+double F = 64.0;
+int main() {
+    double C;
+    cout << "F = " << F << endl;
+    // TODO 请补全代码
+    C = (F-32)/1.8; // 由F=C∗1.8+32反推出C = (F-32)/1.8
+    cout << "C = " << C << endl;
+}
