3333ae10b625a23c4e6a150b1a41f6a5221f67a2
lcds <1228421675@qq.com>
2022-04-13 19:16:16 +0800

    简单排序算法

--- C++算法初级/排序算法/插入排序.cpp
+++ C++算法初级/排序算法/插入排序.cpp
@@ -1,33 +1,31 @@
 #include <bits/stdc++.h>
-using namespace std; 
-
-void bubble_sort(int a[],int n)
-{
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n-i-1;j++)
-        {
-            if(a[j]>a[j+1])swap(a[j],a[j+1]);
-        }
-    }
-}
-
-void display(int a[],int n)
-{
-    for(int i=0;i<n;i++)
-    cout<<a[i]<<" ";
-}
+#define N 1550
+using namespace std;
+int a[N], n;
 
 int main() {
-    // 请补全代码，实现题目功能
-    int n;
-    cin>>n;
-    int a[1005];
-    memset(a,0,sizeof(a));
-    for(int i=0;i<n;i++)cin>>a[i];
+    // 输入
+    cin >> n; 
+    for (int i = 1; i <= n; ++i) cin >> a[i];
+	
+    // 插入排序
+    for (int i = 2; i <= n; ++i) {    // 按照第2个到第n个的顺序依次插入
+        int j, x = a[i];    // 先将i号元素用临时变量保存防止被修改。
 
-    bubble_sort(a,n);
-    display(a,n);
-
+        // 插入过程，目的是空出分界线位置j，使得所有<j的部分<=x，所有>j的部分>x。
+        // 循环维持条件，j>1，并且j前面的元素>x。
+        for (j = i; j > 1 && a[j - 1] > x; --j) {   
+            // 满足循环条件，相当于分界线应向前移，
+            // 分界线向前移，就等于将分界线前面>x的元素向后移
+            a[j] = a[j - 1];              
+                                                    
+        }
+        // 找到分界线位置，插入待插入元素x
+        a[j] = x;                         
+    }
+	
+    // 输出
+    for (int i = 1; i <= n; ++i) cout << a[i] << ' ';
+    cout << endl;
     return 0;
 }
